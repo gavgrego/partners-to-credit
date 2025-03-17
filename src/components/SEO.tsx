f549f43d7344bc45879f551e735f320535cf2af4
Gavin Gregory <gavgrego@gmail.com>
2025-03-17 03:00:25 -0700

    update seo

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -10,7 +10,7 @@ interface SEOProps {
 
 const SEO = ({
   title = 'partnersto.credit - Credit Card Points Transfer Guide',
-  description = 'Find the best credit card transfer partners and current point transfer bonuses. Compare transfer ratios across American Express, Chase, Capital One, Citi, and BILT.',
+  description = 'Find the best credit card transfer partners and current point transfer bonuses. Compare transfer ratios across American Express, Chase, Capital One, Citi, and BILT. See the list of credit card transfer partners and our interactive chart to see which airlines and hotels you can transfer your credit card points to.',
   canonical = 'https://partnersto.credit',
   type = 'website',
   name = 'partnersto.credit',
